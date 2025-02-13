d893901972f6f590ce8f62c07b421d302f371d25
mmt9633@GGN-MAC-9633 <Kshitij.Jain@go-mmt.com>
2025-02-13 15:18:02 +0530

    FLT-15138737: Added changes for retrieve pnr of Voyzant supplier [copilot:300]

--- VOYZANT-Pnr-Entity/src/main/java/com/mmt/flights/entity/pnr/retrieve/response/FlightSegmentList.java
+++ VOYZANT-Pnr-Entity/src/main/java/com/mmt/flights/entity/pnr/retrieve/response/FlightSegmentList.java
@@ -1,9 +1,10 @@
 package com.mmt.flights.entity.pnr.retrieve.response;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class FlightSegmentList {
+    @JsonProperty("FlightSegment")
     private List<FlightSegment> FlightSegment;
 
     public List<FlightSegment> getFlightSegment() {
