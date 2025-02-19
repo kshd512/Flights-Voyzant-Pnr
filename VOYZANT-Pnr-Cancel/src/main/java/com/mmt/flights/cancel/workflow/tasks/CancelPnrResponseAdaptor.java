8ca7235b511485bd099e8a79e9da62085bc43052
MMT9633_mmt <kshitij.jain@gmail.com>
2025-02-19 18:22:07 +0530

    FLT-15138737: Added changes for implementing cancel API of Voyzant supplier [copilot:30]

--- VOYZANT-Pnr-Cancel/src/main/java/com/mmt/flights/cancel/workflow/tasks/CancelPnrResponseAdaptor.java
+++ VOYZANT-Pnr-Cancel/src/main/java/com/mmt/flights/cancel/workflow/tasks/CancelPnrResponseAdaptor.java
@@ -1,38 +1,93 @@
 package com.mmt.flights.cancel.workflow.tasks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mmt.api.rxflow.FlowState;
 import com.mmt.api.rxflow.task.MapTask;
 import com.mmt.flights.common.constants.FlowStateKey;
-import com.mmt.flights.supply.cancel.v4.request.SupplyPnrCancelRequestDTO;
+import com.mmt.flights.entity.cancel.response.CancelPnrResponse;
+import com.mmt.flights.entity.cancel.response.OrderViewRS;
+import com.mmt.flights.entity.cancel.response.Response;
+import com.mmt.flights.entity.split.response.AirSplitPnrResponse;
 import com.mmt.flights.supply.cancel.v4.response.SupplyPnrCancelResponseDTO;
+import com.mmt.flights.supply.cancel.v4.response.SupplyPnrCancelResponseMetaDataDTO;
+import com.mmt.flights.supply.common.SupplyErrorDetailDTO;
 import com.mmt.flights.supply.common.enums.SupplyStatus;
 import org.springframework.stereotype.Component;
 
-import static com.mmt.flights.common.constants.CommonConstants.EMPTY_STRING;
+import static com.mmt.flights.common.constants.CommonConstants.SERVICE_NAME;
+import static com.mmt.flights.common.constants.CommonConstants.SUPPLIER_NAME;
 
 @Component
 public class CancelPnrResponseAdaptor implements MapTask {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public FlowState run(FlowState state) throws Exception {
-
-        SupplyPnrCancelRequestDTO supplyPnrRequestDTO = state.getValue(FlowStateKey.REQUEST);
         String cancelResponse = state.getValue(FlowStateKey.CANCEL_PNR_RESPONSE);
-        SupplyPnrCancelResponseDTO.Builder response = SupplyPnrCancelResponseDTO.newBuilder();
-        if (response.getCancellationStatus() != null ) {
-            response.setCancellationStatus(SupplyStatus.SUCCESS);
-            response.setRefundStatus(SupplyStatus.SUCCESS);
-                response.setSplitPnr(state
-                        .getValue(FlowStateKey.GetPnr));
-        } else {
-            response.setCancellationStatus(SupplyStatus.FAILURE);
-            response.setRefundStatus(SupplyStatus.FAILURE);
-        }
-        
+        String splitPnrResponse = state.getValue(FlowStateKey.SPLIT_PNR_RESPONSE);
 
-		response.setPenalty(EMPTY_STRING);
+        // Parse cancel response
+        CancelPnrResponse cancelPnrResponse = objectMapper.readValue(cancelResponse, CancelPnrResponse.class);
+        
+        // Build response
+        SupplyPnrCancelResponseDTO.Builder response = SupplyPnrCancelResponseDTO.newBuilder();
+        
+        // Set statuses based on cancel response
+        OrderViewRS orderViewRS = cancelPnrResponse.getOrderViewRS();
+        if (orderViewRS != null && orderViewRS.getResponse() != null && !orderViewRS.getResponse().isEmpty()) {
+            Response cancelResponseDetails = orderViewRS.getResponse().get(0);
+            
+            if ("SUCCESS".equalsIgnoreCase(cancelResponseDetails.getStatus()) &&
+                "CANCELLED".equalsIgnoreCase(cancelResponseDetails.getBookingStatus())) {
+                response.setCancellationStatus(SupplyStatus.SUCCESS);
+                response.setRefundStatus(SupplyStatus.SUCCESS);
+                
+                // Set split PNR reference if available - extract just the split PNR value
+                if (splitPnrResponse != null) {
+                    AirSplitPnrResponse splitPnrResponseObj = objectMapper.readValue(splitPnrResponse, AirSplitPnrResponse.class);
+                    if (splitPnrResponseObj != null && splitPnrResponseObj.getAirSplitPnrRS() != null) {
+                        String splitPnr = splitPnrResponseObj.getAirSplitPnrRS().getSplitedGdsBookingReference();
+                        if (splitPnr != null) {
+                            response.setSplitPnr(splitPnr);
+                        }
+                    }
+                }
+            } else {
+                response.setCancellationStatus(SupplyStatus.FAILURE);
+                response.setRefundStatus(SupplyStatus.FAILURE);
+                
+                // Add error details
+                SupplyErrorDetailDTO.Builder errorBuilder = SupplyErrorDetailDTO.newBuilder();
+                errorBuilder.setEc("CANCEL_FAILED");
+                errorBuilder.setEm(cancelResponseDetails.getMsg());
+                errorBuilder.setEd(cancelResponseDetails.getMsg());
+                errorBuilder.setStatusCode("500");
+                errorBuilder.setSn(SERVICE_NAME);
+                response.addErr(errorBuilder.build());
+            }
+        } else {
+            response.setCancellationStatus(SupplyStatus.FAILURE); 
+            response.setRefundStatus(SupplyStatus.FAILURE);
+            
+            // Add error for invalid response
+            SupplyErrorDetailDTO.Builder errorBuilder = SupplyErrorDetailDTO.newBuilder();
+            errorBuilder.setEc("INVALID_RESPONSE");
+            errorBuilder.setEm("Invalid or empty cancel response received");
+            errorBuilder.setEd("Invalid or empty cancel response received");
+            errorBuilder.setStatusCode("500");
+            errorBuilder.setSn(SERVICE_NAME);
+            response.addErr(errorBuilder.build());
+        }
+
+        // Set metadata
+        SupplyPnrCancelResponseMetaDataDTO.Builder metaBuilder = SupplyPnrCancelResponseMetaDataDTO.newBuilder();
+        metaBuilder.setServiceName(SERVICE_NAME);
+        metaBuilder.setSupplierName(SUPPLIER_NAME);
+        response.setMeta(metaBuilder.build());
 
         return state.toBuilder()
-                .addValue(FlowStateKey.PNR_CANCEL_RESPONSE,response.build())
+                .addValue(FlowStateKey.RESPONSE, response.build())
                 .build();
     }
 }
