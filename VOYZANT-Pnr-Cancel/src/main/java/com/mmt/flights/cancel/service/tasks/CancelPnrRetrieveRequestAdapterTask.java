3a321ca7d5e81e0ae3f463791245c2c9a6a66856
MMT9633_mmt <kshitij.jain@gmail.com>
2025-03-12 16:58:54 +0530

    FLT-15138737: Added changes for implementing VOYZANT APIs [copilot:0]

--- VOYZANT-Pnr-Cancel/src/main/java/com/mmt/flights/cancel/service/tasks/CancelPnrRetrieveRequestAdapterTask.java
+++ VOYZANT-Pnr-Cancel/src/main/java/com/mmt/flights/cancel/service/tasks/CancelPnrRetrieveRequestAdapterTask.java
@@ -9,15 +9,11 @@ import com.mmt.flights.entity.common.Query;
 import com.mmt.flights.entity.pnr.retrieve.request.OrderRetreiveRQ;
 import com.mmt.flights.entity.pnr.retrieve.request.OrderRetrieveRequest;
 import com.mmt.flights.supply.cancel.v4.request.SupplyPnrCancelRequestDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CancelPnrRetrieveRequestAdapterTask implements MapTask {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CancelPnrRetrieveRequestAdapterTask.class);
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -27,26 +23,14 @@ public class CancelPnrRetrieveRequestAdapterTask implements MapTask {
 
     @Override
     public FlowState run(FlowState flowState) throws Exception {
-        LOGGER.info("Starting CancelPnrRetrieveRequestAdapterTask");
-        
         SupplyPnrCancelRequestDTO supplyPnrRequestDTO = flowState.getValue(FlowStateKey.REQUEST);
-        
-        // Create Order Retrieve Request
         OrderRetrieveRequest orderRetrieveRequest = createOrderRetrieveRequest(supplyPnrRequestDTO);
-        
-        // Convert to JSON
         String retrievePnrRequest = objectMapper.writeValueAsString(orderRetrieveRequest);
-        
-        LOGGER.info("CancelPnrRetrieveRequestAdapterTask completed successfully");
-        
         return flowState.toBuilder()
                 .addValue(FlowStateKey.SUPPLIER_PNR_RETRIEVE_REQUEST, retrievePnrRequest)
                 .build();
     }
 
-    /**
-     * Creates an OrderRetrieveRequest object
-     */
     private OrderRetrieveRequest createOrderRetrieveRequest(SupplyPnrCancelRequestDTO supplyPnrRequestDTO) {
         OrderRetrieveRequest orderRetrieveRequest = new OrderRetrieveRequest();
         OrderRetreiveRQ orderRetreiveRQ = createOrderRetrieveRQ(supplyPnrRequestDTO);
@@ -54,25 +38,14 @@ public class CancelPnrRetrieveRequestAdapterTask implements MapTask {
         return orderRetrieveRequest;
     }
 
-    /**
-     * Creates an OrderRetreiveRQ object with document, party, and query
-     */
     private OrderRetreiveRQ createOrderRetrieveRQ(SupplyPnrCancelRequestDTO supplyPnrRequestDTO) {
         OrderRetreiveRQ orderRetreiveRQ = new OrderRetreiveRQ();
-        
-        // Use CommonDocumentService for document and party
         orderRetreiveRQ.setDocument(commonDocumentService.createDocument());
         orderRetreiveRQ.setParty(commonDocumentService.createParty());
-        
-        // Set query with PNR details
         orderRetreiveRQ.setQuery(createQuery(supplyPnrRequestDTO));
-        
         return orderRetreiveRQ;
     }
 
-    /**
-     * Creates a Query object with the PNR information
-     */
     private Query createQuery(SupplyPnrCancelRequestDTO supplyPnrRequestDTO) {
         Query query = new Query();
         String pnr = supplyPnrRequestDTO.getRequestCore().getSupplierPnr();
